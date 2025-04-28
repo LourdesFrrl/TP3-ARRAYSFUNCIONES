efc55644b6fd03d3ae40a5800c786861f229bd29
LourdesFrrl <ferraiolimarialourdesfacultad@gmail.com>
2025-04-28 19:30:22 -0300

    agrege una idea de como hacer el pto 3

---
+++ JS/dados.js
@@ -0,0 +1,25 @@
+/*
+3- Escribir un script que simule el lanzamiento de dos dados. Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces esta operación.
+*/
+
+/*
+let array que guarda los numeros
+
+for(let i=0; i>50; i++ ){
+let numero = math.randon
+let numero 2= math.randon  
+while(numero <=6 && numero2<=6){
+let suma = numero + numero 2
+multiplicacion= push.array
+guardar el numero en el array
+}
+}
+
+buscar una funcion o algo que cuente cuantas veces aparece el numero en el array
+ armar una tabla
+
+
+
+
+
+*/
